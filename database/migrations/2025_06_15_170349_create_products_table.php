0b0a644199e353be6df1beacb2d0697ff2c56c68
yondraseptian <yondraseptian@gmail.com>
2025-06-28 22:02:19 +0700

    fix bug img can be null

--- database/migrations/2025_06_15_170349_create_products_table.php
+++ database/migrations/2025_06_15_170349_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->decimal('price', 8, 2);
             $table->foreignId('category_id')->constrained()->onDelete('cascade');
-            $table->string('image');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
